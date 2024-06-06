4a372557651d0d1f0514da0b725afb6904debb95
Stephen Oduh <stevihiene@gmail.com>
2024-06-06 22:39:24 +0100

    my advanced api task2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,21 +14,13 @@ def number_of_subscribers(subreddit):
     """Returns the total number of subscribers
     for a given subreddit.
     """
-    # Set the Default URL strings
+
     base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
-
-    # Set an User-Agent
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,subreddit=subreddit)
     user_agent = {'User-Agent': 'Python/requests'}
+    res = requests.get(api_uri, headers=user_agent,allow_redirects=False)
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
     if res.status_code in [302, 404]:
         return 0
 
-    # Returns the total subscribers of the subreddit
     return res.json().get('data').get('subscribers')

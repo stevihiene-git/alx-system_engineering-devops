1c0e8466b467c6cfb9790bc63d211971e440aaab
Stephen Oduh <stevihiene@gmail.com>
2024-08-14 22:55:00 +0100

    my advance api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,13 +7,14 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /Stephen/Oduh)"
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
+
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:

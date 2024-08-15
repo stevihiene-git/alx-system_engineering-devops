090e10b90d540c4ac4d54a07fe377865a93c8394
Stephen Oduh <stevihiene@gmail.com>
2024-08-15 14:37:32 +0100

    afresh

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+# Fix 500 error when a GET HTTP method is requested to Apache web serve.........r
 
 exec {'replace':
   provider => shell,
